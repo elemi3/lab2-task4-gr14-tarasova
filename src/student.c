6caa6f84ac606417cb06e2fa14fa87d0ec4d9f2a
elemi3 <elnaratarasova68@gmail.com>
2026-03-01 07:38:17 +0300

    Add detailed comments to student.c

--- src/student.c
+++ src/student.c
@@ -1,17 +1,29 @@
+/* student.c */
+/* Подключение стандартной библиотеки для ввода-вывода */
 #include <stdio.h>
+/* Подключение заголовочного файла с объявлением структур */
 #include "student.h"
 
-void sstudent (void) {
+/**
+ * Функция для создания и вывода информации о студенте
+ * Использует структуры StudentData и stu_address
+ */
+void sstudent(void) {
+    /* Объявление переменной типа struct StudentData */
     struct StudentData student;
-   
-    student.stu_name = "Tarasova";   
-    student.stu_id = 123456;         
-    student.stu_age = 19;                
-    student.stuAddress.street = "4, pr. Nezavisimosti";
-    student.stuAddress.state = "Minskaya";
-    student.stuAddress.city = "Minsk";
-    student.stuAddress.country = "Belarus";
-
+    
+    /* Присвоение значений полям структуры StudentData */
+    student.stu_name = "Tarasova";      // Фамилия студента
+    student.stu_id = 123456;             // Номер студенческого билета
+    student.stu_age = 19;                 // Возраст студента
+    
+    /* Заполнение вложенной структуры с адресом */
+    student.stuAddress.street = "4, pr. Nezavisimosti";  // Улица и дом
+    student.stuAddress.state = "Minskaya";                // Область
+    student.stuAddress.city = "Minsk";                     // Город
+    student.stuAddress.country = "Belarus";                // Страна
+    
+    /* Вывод информации на экран */
     printf("Hello, %s!", student.stu_name);
     printf("\nStudent Id is: %d", student.stu_id);
     printf("\nStudent Age is: %d", student.stu_age);
